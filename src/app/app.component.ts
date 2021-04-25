97ff8d7b912a53a084f765c193d07526f88cc6f2
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-04-26 00:08:41 +0200

    Loading products from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from './http.service';
+import {Product} from './product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'products';
+  products: Product[] = [];
+
+  constructor(private httpService: HttpService) {
+  }
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.httpService.getRequest('https://crudcrud.com/api/984288ef31d04a7f822f4d94f05bf11a/products')
+      .subscribe((response) => {
+        this.products = response;
+      });
+  }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Product} from '../product';
 
 @Component({
   selector: 'product',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  @Input() product: Product = {} as Product;
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
